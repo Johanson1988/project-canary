88dd57216a19eef6bbfb7b934a3fcd6dc0ebab92
Johann <johanson88@gmail.com>
2019-12-10 22:53:40 +0100

    folder structure created

--- routes/game/game-index.js
+++ routes/game/game-index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/auth', require('./auth'));
-router.use('/game', require('./game/game-index.js'));
 
 module.exports = router;
