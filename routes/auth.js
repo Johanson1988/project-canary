00c7e15c58a8a160c4f23f3d6356561ae330e2d0
Johann <johanson88@gmail.com>
2019-12-10 18:12:08 +0100

    Test with Postman auth working

--- routes/auth.js
+++ routes/auth.js
@@ -12,31 +12,82 @@ const {
   validationLoggin,
 } = require('../helpers/middlewares');
 
-// GET '/auth/me'
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - if yes, send the response with user info (available on req.session.currentUser)
 
-// POST '/auth/login'
-//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
-//  - check that `password` and `username` are sent - using helper function
-//  - check if user exists in the DB
-//  - if user doesn't exist - forward the error to the error middleware using `next()`
-//  - if user exists get assign the user document to `req.session.currentUser` and then send  json response
+//		routes/auth.js
 
-// POST '/auth/signup'
-//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
-//  - check that `password` and the `username` are sent - using helper function
+//  GET    '/me'
+router.get('/me', isLoggedIn, (req, res, next) => {
+  req.session.currentUser.password = '*';
+  res.json(req.session.currentUser);
+});
 
-// - check if `username` already exists in the DB
-//  - if username exists - forward the error to the error middleware using `next()`
-//  - else if username doesn't exists hash the password and create new user in the DB
-//  - then assign the  new user document to `req.session.currentUser` and then send  json response
+//  POST    '/login'
+router.post('/login', isNotLoggedIn, validationLoggin, async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username }) ;
+    if (!user) {
+      next(createError(404));
+    } 
+    else if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res
+        .status(200)
+        .json(user);
+      return 
+    } 
+    else {
+      next(createError(401));
+    }
+  } 
+  catch (error) {
+    next(error);
+  }
+},
+);
 
-// POST '/auth/logout'
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - destroy the session
-//  - set status code and send the response back
+//  POST    '/signup'
+router.post('/signup', isNotLoggedIn, validationLoggin, async (req, res, next) => {
+  const { username, password } = req.body;
 
-// GET '/auth/private'   --> Only for testing - Same as /me but it returns a message instead
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - set status code and send the json message response back
+  try {																									 // projection
+    const usernameExists = await User.findOne({ username }, 'username');
+    
+    if (usernameExists) return next(createError(400));
+    else {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = await User.create({ username, password: hashPass });
+      req.session.currentUser = newUser;
+      res
+        .status(200)  //  OK
+        .json(newUser);
+    }
+  } 
+  catch (error) {
+    next(error);
+  }
+},
+);
+//		routes/auth.js
+
+//  POST    '/logout'
+router.post('/logout', isLoggedIn, (req, res, next) => {
+  req.session.destroy();
+  res
+    .status(204)  //  No Content
+    .send();
+  return; 
+});
+
+//		routes/auth.js
+
+//  GET    '/private'   --> Only for testing - Same as `/me` but it returns a message instead
+router.get('/private', isLoggedIn, (req, res, next) => {
+  res
+    .status(200)  // OK
+    .json({ message: 'Test - User is logged in'});
+});
+
+
+module.exports = router;
