4fb4a007999b4166191f0f56c7d67c6cf3a5ce1d
Johann <johanson88@gmail.com>
2019-12-11 00:33:09 +0100

    game route OK Pass postman test

--- routes/game/game.js
+++ routes/game/game.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Game = require('../../models/Game');
-
+ 
 
 router.post('/', (req,res,next) => {
+    const { name, createdBy } = req.body;    
+    Game.create({name,createdBy})
+        .then(() =>res.status(201).json())        
+        .catch(err => res.status(400).json(err))
+
     
 });
 
