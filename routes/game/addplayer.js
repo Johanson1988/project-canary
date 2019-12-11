6c8da871fd5321e4f84a3b6d58df90b8edf8db3f
Johann <johanson88@gmail.com>
2019-12-11 14:51:57 +0100

    commit to update remote repo

--- routes/game/addplayer.js
+++ routes/game/addplayer.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../../models/Game');
 
-router.patch('/', (req,res,next) => {
-    console.log(req.body);
+router.patch('/', (req,res,next) => {  //TODO update readme with body and new route
+    const {playerSocketId,gameId} = req.body;
+    
     res.status(200).json('holaa')
   });
 
