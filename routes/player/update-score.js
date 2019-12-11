743de9a978fd3c44d28fcb66f91d615f3b19099f
Johann <johanson88@gmail.com>
2019-12-11 17:35:43 +0100

    update-score route OK

--- routes/player/update-score.js
+++ routes/player/update-score.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const Player = require('./../../models/Player');
 
 router.patch('/', (req,res,next) => {  //TODO update this route in readme merge both routes addplayer and patch player
-    console.log(req.body);
-    res.status(200).send();
+    const {playerId:_id, score} = req.body;
+    Player.updateOne({_id},{score})
+        .then(()=>res.status(200).send())
+        .catch(err => res.status(400).json(err));    
 });
 
 module.exports = router;
