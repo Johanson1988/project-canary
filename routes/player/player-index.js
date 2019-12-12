e3ba582a715fa3e6f06c91b9742bff9208d493f8
Johann <johanson88@gmail.com>
2019-12-12 15:38:27 +0100

    Add route player/create in progreess

--- routes/player/player-index.js
+++ routes/player/player-index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.use('/addplayer/image', require('./add-image'));
 router.use('/update-score', require('./update-score'));
+router.use('/create', require('./create-player'));
 router.use('/addplayer', require('./add-player')); //TODO update this route in readme
 
 
