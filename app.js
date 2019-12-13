13c2f7627cec833287c749472945d5758b8a8cd2
Johann <johanson88@gmail.com>
2019-12-13 16:21:56 +0100

    Add sockets to app.js

--- app.js
+++ app.js
@@ -42,6 +42,28 @@ app.use(
 //   next();
 // });
 
+//SOCKETS
+
+const http = require('http');
+const io = require('socket.io')();
+
+const PORT = process.env.SOCKETPORT || 9000;
+const server = http.createServer();
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected.`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+  });
+});
+
+server.listen(PORT);
+
+
+
 // SESSION MIDDLEWARE
 app.use(
   session({
