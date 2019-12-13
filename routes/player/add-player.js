6ae5522e30a3b505d3168f5f20eb3414effec9b2
Johann <johanson88@gmail.com>
2019-12-13 17:56:59 +0100

    add player returning player object

--- routes/player/add-player.js
+++ routes/player/add-player.js
@@ -4,14 +4,15 @@ const Game = require('./../../models/Game');
 const Player = require('./../../models/Player');
 
 router.post('/', (req,res,next) => {  //TODO update this route in readme merge both routes addplayer and patch player
+    console.log(req.body);
     const {player} = req.body;
     Game.findOne({_id:player.gameId})
         .then(() => {
             Player.create(player)
-                .then(player =>{                
-                    Game.updateOne({_id:player.gameId}, {$push:{players:player._id}})
-                        .then(() => {
-                            res.status(201);
+            .then(player =>{                
+                Game.updateOne({_id:player.gameId}, {$push:{players:player._id}})
+                .then(() => {                    
+                            res.status(201).json(player);
                         })
                         .catch(err => res.status(400).json(err));
                 } )
