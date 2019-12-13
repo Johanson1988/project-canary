43c8f927fde44a0169e3ce319be10b18e5d2125e
Johann <johanson88@gmail.com>
2019-12-13 15:59:56 +0100

    start finish game routes OK

--- routes/game/start-game.js
+++ routes/game/start-game.js
@@ -11,7 +11,9 @@ const {
   } = require('../../helpers/middlewares');
 
 router.patch('/', (req,res,next) => { //TODO update route in readme
+
     const {gameId:_id} = req.body;    
+    console.log(_id);
     Game.updateOne({_id},{gameStatus:'Running'}) //TODO update this
         .then(() =>res.status(200).send())
         .catch(err => res.status(400).json(err));
