ce80ab567ccd743c06b5967ccc54cf1b6f1bc5f2
Johann <johanson88@gmail.com>
2019-12-15 23:21:06 +0100

    points array created

--- routes/game/start-game.js
+++ routes/game/start-game.js
@@ -5,11 +5,20 @@ const Game = require('../../models/Game');
 router.patch('/', (req,res,next) => { //TODO update route in readme
     const {gameId:_id} = req.body;
     const io = req.app.locals.io;
-    var i= 0;
+    const points = [];
+    var i = 0;
     
-    Game.findOneAndUpdate({_id}, {gameStatus:'Running'}, {new:true, useFindAndModify:false}) //TODO update this
-        .then((currentGame) =>{   
-            console.log(currentGame.players.length);
+    Game.findOneAndUpdate({_id}, {gameStatus:'Running'}, {new:true, useFindAndModify:false})  //TODO update this
+        .populate('questions')
+        .populate('players')  
+        .then((currentGame) =>{
+            currentGame.questions.forEach((question, firstIndex) => {
+                points.push([]);
+                currentGame.players.forEach((player, index) => {
+                        points[firstIndex].push(index * 100 + 100);
+                        console.log(points[index],index * 100 + 100);
+                    });
+                });
             const gameRoom = io.of('/' + _id);
             console.log('Game Started: ', _id);
             io.to(_id).emit('game-started', {message:'The game has started'});
@@ -24,19 +33,16 @@ router.patch('/', (req,res,next) => { //TODO update route in readme
                     clearInterval(intervalId);
                     return;
                 }
-                console.log('New question ',i);
-                
+                console.log('New question ',i);                
                 i++;
                 io.to(_id).emit('new-question');
-             },5000)
+             },5000);
          //   console.log(gameRoom);
-         i++;
-         console.log('i',i);
             //io.emit('game-started', {message:'The game has started'});
 
             //io.emit('game-started', {message:'The game has started'});
             
-            res.status(200).send();
+        res.status(200).send();
 
         })
         .catch(err => res.status(400).json(err));
