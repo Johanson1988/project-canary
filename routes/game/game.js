6fc901e771f737b35fb5a01913a04420063d4cb5
Johann <johanson88@gmail.com>
2019-12-16 17:11:07 +0100

    updated URL to localhost

--- routes/game/game.js
+++ routes/game/game.js
@@ -66,7 +66,8 @@ router.post('/',validationGame, (req,res,next) => {
         Game.create({name,createdBy,questions:finalQuestionsList,gameStatus})
               .then((createdGame) => {
                 const {_id} = createdGame;
-                QRCode.toDataURL('https://iron-canary.herokuapp.com/entername/id=' + _id, (err, qrCode) => {
+                //https://iron-canary.herokuapp.com
+                QRCode.toDataURL('http://localhost:3000/entername/id=' + _id, (err, qrCode) => {
                   if (!err) {                    
                     createdGame.qrCode = qrCode;
                     Game.findOneAndUpdate({_id}, {qrCode}, {new:true, useFindAndModify:false})
