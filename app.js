92e1b078c1f85032f006245b8b99a54ee9fb5f32
Johann <johanson88@gmail.com>
2019-12-17 15:22:31 +0100

    added127.0.0.1 to cors

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN,'https://iron-canary.herokuapp.com','http://192.168.1.109:3000'],
+    origin: [process.env.PUBLIC_DOMAIN,'http://127.0.0.1:5000','https://iron-canary.herokuapp.com','http://192.168.1.109:3000'],
   }),
 );
 // app.use((req, res, next) => {
@@ -139,7 +139,7 @@ io.on('connection', socket =>{
   })
 
 });
-server.listen(PORT);
+//server.listen(PORT);
 app.locals.io = io;
 
 
