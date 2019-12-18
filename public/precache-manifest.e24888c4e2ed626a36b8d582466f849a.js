fc9e600a751e1c86a971432a668166cf3bfa1908
Johann <johanson88@gmail.com>
2019-12-18 10:27:43 +0100

    updated front end

---
+++ public/precache-manifest.e24888c4e2ed626a36b8d582466f849a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62f48365d1093f35c7930b867e6f238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675b960885b7c8e833e5",
+    "url": "/static/css/main.4a226c10.chunk.css"
+  },
+  {
+    "revision": "44e41f3fe531098d91dc",
+    "url": "/static/js/2.4205deba.chunk.js"
+  },
+  {
+    "revision": "29a0f7d19db06398fb9ff3b5e407b9aa",
+    "url": "/static/js/2.4205deba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "675b960885b7c8e833e5",
+    "url": "/static/js/main.e6897a96.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  },
+  {
+    "revision": "2db0e8cf001eddd3c9a352b4d0c76fe5",
+    "url": "/static/media/corner-up-left.2db0e8cf.svg"
+  },
+  {
+    "revision": "664c1be5f0066b823b8643a5e3d5fcf6",
+    "url": "/static/media/edit.664c1be5.svg"
+  },
+  {
+    "revision": "827d2506ec060e476923ee2954fc15e4",
+    "url": "/static/media/exit.827d2506.svg"
+  },
+  {
+    "revision": "464c6dc06b286ca200b4f42d18914b9f",
+    "url": "/static/media/plus-square.464c6dc0.svg"
+  },
+  {
+    "revision": "be84ac99ae1e008f64fbfde0437630c8",
+    "url": "/static/media/trash-2.be84ac99.svg"
+  }
+]);
